4c468a5ff520ef30ef41cd276b22aae720280627
Jacob <jacobedwardhaig@gmail.com>
2025-05-12 23:13:49 -0400

    Added profanity Filter

---
+++ README.md
@@ -0,0 +1,4 @@
+# Commands
+pip install uv --upgrade
+uv sync
+uv run main.py --id FnJ-B_A0OPw -v 0.25
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import argparse
 
 import pytchat
 from yapper import Yapper, PiperSpeaker, PiperQuality, PiperVoiceGB
+from profanityfilter import ProfanityFilter
+
+pf = ProfanityFilter()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--id", required=True, help="Your Video ID")
@@ -24,8 +27,12 @@ def get_speaker(custom_volume=0.25):
 yapper = get_speaker(custom_volume=volume)
 
 
-def say(user: str, saying: str) -> None:
-    yapper.yap(f"{user} said: {saying}")
+def say(username: str, saying: str) -> None:
+    full_saying = f"{username} said: {saying}"
+    if username.lower() == "jacob haig":
+        full_saying = f"Wisward said: {saying}"
+
+    yapper.yap(pf.censor(full_saying))
 
 
 def main():
@@ -37,10 +44,16 @@ def main():
         print(" === Youtube Stream is not connected! ===")
         return
 
-    while chat.is_alive():
-        for c in chat.get().sync_items():
-            print(f"{c.datetime} [{c.author.name}]- {c.message}")
-            say(c.author.name, c.message)
+    while chat.is_alive() and not chat.is_replay():
+        # Check if ctrl+c is pressed
+        try:
+            for c in chat.get().sync_items():  # this is waiting for the next message. We should switch to async
+                print(f"{c.datetime} [{c.author.name}]- {c.message}")
+                say(c.author.name, c.message)
+
+        except KeyboardInterrupt:
+            break
+    print(" === Youtube Stream is disconnected! ===")
 
 
 if __name__ == '__main__':
